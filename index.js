47da90ca64b2f82f8d3acab14da0140b4b8a6250
thelewisndegwa <lewiskanyi77@gmail.com>
2023-03-20 20:11:03 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(price){
+  if (price <=400){
+    return 'This one is on me!'
+  }
+  else if (price <=2499){
+    return  'I will gladly take your thirty bucks.'
+  }
+  else (price >2500)
+    return 'No can do.'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet());
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return 'Ok, sounds good.'
+  }
+  else (city !== "NYC")
+    return 'No go.'
+  }
+console.log(ternaryCheckCity())
+function switchOnCharmFromTip(trip){
+if (trip === "generous"){
+  return 'Thank you so much.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (trip === "not as generous"){
+  return 'Thank you.'
+}
+else (trip !== "any thing else")
+  return 'Bye.'
+}
+console.log(switchOnCharmFromTip())
+console.log(ternaryCheckCity())
+function switchOnCharmFromTip(trip){
+if (trip === "generous"){
+  return 'Thank you so much.'
+}
+else if (trip === "not as generous"){
+  return 'Thank you.'
+}
+else (trip !== "any thing else")
+  return 'Bye.'
 }
